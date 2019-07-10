3f6e64ea7cd1ce17b8033efdbfaaa3a4ea15d670
Daniel Weinman <namniewd@gmail.com>
2019-07-09 18:33:47 -0700

    fixing styles

--- client/src/components/TableDataDrop/TableDataDrop.js
+++ client/src/components/TableDataDrop/TableDataDrop.js
@@ -4,7 +4,10 @@ import axios from 'axios';
 import './TableDataDrop.css';
 
 const TableDataDrop = props => {
+  // tableInfo hook.  User will copy paste table draw from pokernews here
   const [tableInfo, setTableInfo] = useState('0');
+
+  // options for how much data to grab, toggled by checkboxes in render
   const [options, setOptions] = useState({
     Name: true,
     Chips: true,
@@ -14,7 +17,7 @@ const TableDataDrop = props => {
     Buyin: true
   })
   
-
+  // function to toggle options state called by checkbox inputs
   const toggleCheckbox = e => {
     setOptions({
       ...options,
@@ -22,10 +25,12 @@ const TableDataDrop = props => {
     })
   }
 
+  // function to update tableInfo state after raw data pasted in
   const handleChange = e => {
     setTableInfo(e.target.value)
   }
 
+  // function to send post request to backend with tableInfo and options
   const handleSubmit = () => {
     axios.post('http://localhost:5000/api/table', {tableInfo, options})
       .then(res => console.log(res))
@@ -41,18 +46,30 @@ const TableDataDrop = props => {
         onChange={handleChange}
       />
       <div className="options">
-        <input onClick={toggleCheckbox} type="checkbox" name="Name" defaultChecked/>Name
-        <input onClick={toggleCheckbox} type="checkbox" name="Chips" defaultChecked/>Chip Count
-        <input onClick={toggleCheckbox} type="checkbox" name="Nationality" defaultChecked/>Nationality
-        <input onClick={toggleCheckbox} type="checkbox" name="Earnings" defaultChecked/>Total Live Earnings
-        <input onClick={toggleCheckbox} type="checkbox" name="Largest" defaultChecked/>Largest Cash
-        <input onClick={toggleCheckbox} type="checkbox" name="Buyin" defaultChecked/>Average Buyin
+        <h3>What data do you want on your opponents?</h3>
+        {/* fix other 2 lines to be like this one */} 
+        <div className="sub-options">
+          <div className="left-option">
+            <input onClick={toggleCheckbox} type="checkbox" name="Name" defaultChecked/>Name
+          </div>
+          <div className="right-option">
+            <input onClick={toggleCheckbox} type="checkbox" name="Chips" defaultChecked/><span>Chip Count</span>
+          </div>
+        </div>
+        <div className="sub-options">
+          <input onClick={toggleCheckbox} type="checkbox" name="Nationality" defaultChecked/>Nationality
+          <input onClick={toggleCheckbox} type="checkbox" name="Earnings" defaultChecked/>Total Live Earnings
+        </div>
+        <div className="sub-options">
+          <input onClick={toggleCheckbox} type="checkbox" name="Largest" defaultChecked/>Largest Cash
+          <input onClick={toggleCheckbox} type="checkbox" name="Buyin" defaultChecked/>Average Buyin
+        </div>
       </div>
       <button
         className='submit-btn'
         onClick={handleSubmit}
       >
-        Get My Table Draw
+        Get My Opponent's Info!
       </button>
     </div>
   )
