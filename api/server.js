bd0861d1e6747b03e460ff8525fb322bfd513123
Daniel Weinman <namniewd@gmail.com>
2019-07-09 18:04:27 -0700

    backend receiving data, checkboxes working, should be good for sending all table info to backend

--- api/server.js
+++ api/server.js
@@ -1,15 +1,23 @@
 const express = require('express'); 
 const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
-server.get('/api/table', async (req, res) => {
-  const raw_data = req.body;
+server.get('/', (req, res) => {
+  res.status(200).json('running');
+})
+
+server.post('/api/table', async (req, res) => {
+  const { tableInfo, options} = req.body;
   // testing out passing data from front end to server...
-  console.log(raw_data);
+  console.log(tableInfo);
+  console.log(options);
+  res.status(200).json({tableInfo, options});
 })
 
 module.exports = server;
--- client/src/components/TableDataDrop/TableDataDrop.js
+++ client/src/components/TableDataDrop/TableDataDrop.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import './TableDataDrop.css';
 
 const TableDataDrop = props => {
+  const [tableInfo, setTableInfo] = useState('0');
+  const [options, setOptions] = useState({
+    Name: true,
+    Chips: true,
+    Nationality: true,
+    Earnings: true,
+    Largest: true,
+    Buyin: true
+  })
+  
 
-  // TODO: write function here to send axios request with table draw info to backend
-  const handleData = () => {
-    console.log('test');
+  const toggleCheckbox = e => {
+    setOptions({
+      ...options,
+      [e.target.name]: !options[e.target.name]
+    })
+  }
+
+  const handleChange = e => {
+    setTableInfo(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    axios.post('http://localhost:5000/api/table', {tableInfo, options})
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
   }
 
   return (
     <div className="data-drop">
       <textarea 
         className="raw-data"
-        placeholder="Paste Table Draw Here..." 
+        placeholder="Paste Table Draw Here..."
+        value={tableInfo}
+        onChange={handleChange}
       />
+      <div className="options">
+        <input onClick={toggleCheckbox} type="checkbox" name="Name" defaultChecked/>Name
+        <input onClick={toggleCheckbox} type="checkbox" name="Chips" defaultChecked/>Chip Count
+        <input onClick={toggleCheckbox} type="checkbox" name="Nationality" defaultChecked/>Nationality
+        <input onClick={toggleCheckbox} type="checkbox" name="Earnings" defaultChecked/>Total Live Earnings
+        <input onClick={toggleCheckbox} type="checkbox" name="Largest" defaultChecked/>Largest Cash
+        <input onClick={toggleCheckbox} type="checkbox" name="Buyin" defaultChecked/>Average Buyin
+      </div>
       <button
         className='submit-btn'
-        onClick={handleData}
+        onClick={handleSubmit}
       >
         Get My Table Draw
       </button>
